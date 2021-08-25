3b43f66cdf5f4dcbacffb6796763c665bc68efc1
LemonPocky <someone-70@live.com>
2021-08-25 15:47:59 -0700

    Created component for each rating in Profile

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,32 +1,24 @@
 import React from 'react';
-import { Header, Divider, Grid, Image } from 'semantic-ui-react';
+import { Redirect, useParams } from 'react-router-dom';
+import { Header, Divider, Grid } from 'semantic-ui-react';
 
-const userData = {
-  username: 'My Name',
-  email: 'myemail@email.com',
-};
+import ProfileSongRow from '../components/ProfileSongRow';
+import Auth from '../utils/auth';
 
 const Profile = () => {
+  const { username: userParam } = useParams();
+
+  const userData = {
+    username: 'My Name',
+    email: 'myemail@email.com',
+  };
   return (
     <>
       <Header as="h1">Hello {userData.username}!</Header>
 
       <Grid container textAlign="center" celled="internally">
         <Divider horizontal>Recently Rated Songs</Divider>
-        <Grid.Row columns={3}>
-          <Grid.Column>
-            <Image
-              src={`${process.env.PUBLIC_URL}/images/placeholder-square.jpg`}
-              size="small"
-            />
-          </Grid.Column>
-          <Grid.Column>
-            <Header as="h2">Song Title</Header>
-            <Header as="h3">Artist Name</Header>
-            <Header as="h3">Album Title</Header>
-          </Grid.Column>
-          <Grid.Column>5 Stars</Grid.Column>
-        </Grid.Row>
+        <ProfileSongRow />
       </Grid>
     </>
   );
