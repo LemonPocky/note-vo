c0f1cd85f4543330f7961149cab2b031910bd19f
LemonPocky <someone-70@live.com>
2021-08-25 13:18:33 -0700

    Added Ratings fields to Song and User

--- server/models/Song.js
+++ server/models/Song.js
@@ -1,4 +1,5 @@
 const { Schema, model } = require('mongoose');
+const dayjs = require('dayjs');
 
 const SongSchema = new Schema({
   // spotify id
@@ -33,6 +34,32 @@ const SongSchema = new Schema({
   previewUrl: {
     type: String,
   },
+  // If data is expired, it should be refetched from spotify
+  expiration: {
+    type: Date,
+  },
+});
+
+// Creates a virtual to retrieve ratings for this songId
+// To get these ratings, use populate('ratings')
+SongSchema.virtual('ratings', {
+  ref: 'Rating',
+  localField: 'songId',
+  foreignField: 'song',
+});
+
+// middleware to set expiration date when saved
+SongSchema.pre('save', async function (next) {
+  // Set expiration to one week from now and conver to Javascript Date
+  this.expiration = dayjs().add(1, 'week').toDate();
+  next();
+});
+
+// middleware to set expiration date when updated
+SongSchema.pre('updateOne', async function (next) {
+  // Set expiration to one week from now and conver to Javascript Date
+  this.expiration = dayjs().add(1, 'week').toDate();
+  next();
 });
 
 const Song = model('Song', SongSchema);
