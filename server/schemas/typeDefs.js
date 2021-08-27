ba236303b3809cdb7c3fc08e6d1497179c1f305a
LemonPocky <someone-70@live.com>
2021-08-27 10:46:32 -0700

    Created graphql typedefs

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,61 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  type User {
+    _id: ID!
+    username: String!
+    email: String
+    ratings: [Rating]
+  }
+
+  type Song {
+    songId: String!
+    title: String
+    artists: [String]
+    album: Album
+    link: String
+    previewUrl: String
+    expiration: String
+  }
+
+  type Album {
+    title: String
+    image: String
+  }
+
+  type Rating {
+    rating: Int
+    song: Song
+    user: User
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  input SongInput {
+    songId: String!
+    title: String
+    artists: [String]
+    album: Album
+    link: String
+    previewUrl: String
+  }
+
+  type Query {
+    me: User
+    user(username: String!): User
+    song(songId: String): Song
+  }
+
+  type Mutation {
+    addUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+    addRating(songId: String, rating: Int): Rating
+    removeBook(bookId: ID!): User
+    addSong(song: SongInput): Song
+  }
+`;
+
+module.exports = typeDefs;
