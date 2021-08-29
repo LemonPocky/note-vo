90ac19707af4d76d68187b30d834cb97e32113d6
Sam Lloyd <bobdoe784@gmail.com>
2021-08-29 11:12:32 -0700

    add seed scripts for user and ratings

--- server/seeds/index.js
+++ server/seeds/index.js
@@ -1,11 +1,36 @@
 const db = require("../config/connection");
-const { Song } = require("../models");
+const { Song, User, Rating } = require("../models");
 const songSeeds = require("./songSeeds.json");
-
+const userSeeds = require("./userSeeds.json");
+const ratingSeeds = require("./ratingSeeds.json");
 db.once("open", async () => {
   try {
     await db.dropDatabase();
     await Song.create(songSeeds);
+    console.log("all done!");
+    process.exit(0);
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+});
+
+db.once("open", async () => {
+  try {
+    await db.dropDatabase();
+    await User.create(userSeeds);
+    console.log("all done!");
+    process.exit(0);
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+});
+
+db.once("open", async () => {
+  try {
+    await db.dropDatabase();
+    await Rating.create(ratingSeeds);
     console.log("all done!");
     process.exit(0);
   } catch (err) {
