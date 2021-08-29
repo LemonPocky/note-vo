515928fda67105fc8048fe1fd8d087b543894761
Sam Lloyd <bobdoe784@gmail.com>
2021-08-29 16:04:43 -0700

    Move await functions to same try catch block

--- server/seeds/index.js
+++ server/seeds/index.js
@@ -7,29 +7,7 @@ db.once("open", async () => {
   try {
     await db.dropDatabase();
     await Song.create(songSeeds);
-    console.log("all done!");
-    process.exit(0);
-  } catch (err) {
-    console.error(err);
-    process.exit(1);
-  }
-});
-
-db.once("open", async () => {
-  try {
-    await db.dropDatabase();
     await User.create(userSeeds);
-    console.log("all done!");
-    process.exit(0);
-  } catch (err) {
-    console.error(err);
-    process.exit(1);
-  }
-});
-
-db.once("open", async () => {
-  try {
-    await db.dropDatabase();
     await Rating.create(ratingSeeds);
     console.log("all done!");
     process.exit(0);
