8f94d5a62f9e4e904898bb0ba240117e19179eb3
Sam Lloyd <bobdoe784@gmail.com>
2021-08-29 20:01:53 -0700

    Errors removed

--- server/seeds/index.js
+++ server/seeds/index.js
@@ -2,18 +2,16 @@ const db = require("../config/connection");
 const { Song, User, Rating } = require("../models");
 const songSeeds = require("./songSeeds.json");
 const userSeeds = require("./userSeeds.json");
-const ratingSeeds = require("./ratingSeeds.json");
-const songs = Song.create(songSeeds);
 
 db.once("open", async () => {
   try {
     await db.dropDatabase();
-    await songs;
+    const song = await Song.create(songSeeds);
     const users = await User.create(userSeeds);
     const newRating = await Rating.create({
       rating: 3,
       user: users[0]._id,
-      song: songs[0]._id,
+      song: song[0]._id,
     });
     await User.findByIdAndUpdate(users[0]._id, {
       $addToSet: { ratings: newRating._id },
