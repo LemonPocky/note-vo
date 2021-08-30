1e73462622fb0fcc224fef675d07f26f5d456a78
Sam Lloyd <bobdoe784@gmail.com>
2021-08-29 17:40:00 -0700

    Improve rating data script

--- server/seeds/index.js
+++ server/seeds/index.js
@@ -3,12 +3,23 @@ const { Song, User, Rating } = require("../models");
 const songSeeds = require("./songSeeds.json");
 const userSeeds = require("./userSeeds.json");
 const ratingSeeds = require("./ratingSeeds.json");
+const songs = Song.create(songSeeds);
+const users = await User.create(userSeeds);
+const newRating = await Rating.create({
+  rating: 3,
+  user: users[0]._id,
+  song: songs[0]._id,
+});
+
 db.once("open", async () => {
   try {
     await db.dropDatabase();
-    await Song.create(songSeeds);
-    await User.create(userSeeds);
-    await Rating.create(ratingSeeds);
+    await songs;
+    await users;
+    await newRating;
+    await Song.findByIdAndUpdate(user[0]._id, {
+      $addToSet: { ratings: newRating._id },
+    });
     console.log("all done!");
     process.exit(0);
   } catch (err) {
