4ab97152f3b76f82d743eb49291a1f5cedad574b
Sam Lloyd <bobdoe784@gmail.com>
2021-08-29 18:16:33 -0700

    Adjustments made

--- server/seeds/index.js
+++ server/seeds/index.js
@@ -4,20 +4,18 @@ const songSeeds = require("./songSeeds.json");
 const userSeeds = require("./userSeeds.json");
 const ratingSeeds = require("./ratingSeeds.json");
 const songs = Song.create(songSeeds);
-const users = await User.create(userSeeds);
-const newRating = await Rating.create({
-  rating: 3,
-  user: users[0]._id,
-  song: songs[0]._id,
-});
 
 db.once("open", async () => {
   try {
     await db.dropDatabase();
     await songs;
-    await users;
-    await newRating;
-    await Song.findByIdAndUpdate(user[0]._id, {
+    const users = await User.create(userSeeds);
+    const newRating = await Rating.create({
+      rating: 3,
+      user: users[0]._id,
+      song: songs[0]._id,
+    });
+    await User.findByIdAndUpdate(users[0]._id, {
       $addToSet: { ratings: newRating._id },
     });
     console.log("all done!");
